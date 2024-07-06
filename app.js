2dfb655387ea99fbd7fe89a932e5e6f9f89d93fd
sanskar <sanskargupta37081@gmail.com>
2024-07-06 15:22:16 +0530

    Made the connection

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+
+// Create the HTTP server
+const server = http.createServer(app);
+
+// Initialize Socket.io with the server
+const io = socketio(server);
 
 app.get('/', function (req, res) {
     res.send("Hello");
@@ -7,6 +15,6 @@ app.get('/', function (req, res) {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, function () {
+server.listen(PORT, function () {
     console.log(`Server is running on ${PORT}`);
 });
